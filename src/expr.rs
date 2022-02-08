f0be0ccd409fd2abe8fd3189223d214061808eb7
Reto Achermann <achreto@gmail.com>
2022-02-08 15:22:16 -0800

    expr: handling of method calls and field accesses
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/expr.rs
+++ src/expr.rs
@@ -29,7 +29,7 @@
 
 use std::fmt::{self, Display, Write};
 
-use crate::{Formatter, Type};
+use crate::{BaseType, Formatter, MethodParam, Type};
 
 /// Defines an statement
 #[derive(Debug, Clone)]
@@ -40,10 +40,18 @@ pub enum Expr {
     Const(String),
     /// represents a function call
     FnCall { name: String, args: Vec<Expr> },
+    /// represents a method call
+    MethodCall {
+        var: Box<Expr>,
+        method: String,
+        args: Vec<Expr>,
+    },
     /// represents the dereference operator `*(Expr)`
     Deref(Box<Expr>),
     /// represents the address of operationr: `&(Expr)`
     AddrOf(Box<Expr>),
+    /// accesses the field
+    FieldAccess { var: Box<Expr>, field: String },
     /// represents a binary opreation: `a + b`
     BinOp {
         lhs: Box<Expr>,
@@ -57,6 +65,55 @@ pub enum Expr {
 }
 
 impl Expr {
+    pub fn from_method_param(p: &MethodParam) -> Self {
+        Expr::Variable {
+            name: p.name().to_string(),
+            ty: p.to_type(),
+        }
+    }
+
+    pub fn this() -> Self {
+        Expr::Variable {
+            name: "this".to_string(),
+            ty: Type::from_ptr(&Type::new(BaseType::Class(String::from("auto"), vec![]))),
+        }
+    }
+
+    pub fn field_access(var: &Expr, field: &str) -> Self {
+        Expr::FieldAccess {
+            var: Box::new(var.clone()),
+            field: field.to_string(),
+        }
+    }
+
+    pub fn method_call(var: &Expr, method: &str, args: Vec<Expr>) -> Self {
+        Expr::MethodCall {
+            var: Box::new(var.clone()),
+            method: method.to_string(),
+            args: args,
+        }
+    }
+
+    pub fn is_ptr(&self) -> bool {
+        match self {
+            Expr::Variable { ty, .. } => ty.is_ptr(),
+            Expr::Deref(e) => {
+                panic!("handle me!");
+            }
+            Expr::AddrOf(_) => true,
+            Expr::Raw(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_struct(&self) -> bool {
+        match self {
+            Expr::Variable { ty, .. } => ty.is_struct(),
+            Expr::Raw(_) => true,
+            _ => false,
+        }
+    }
+
     pub fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Expr::Variable { name, .. } => write!(fmt, "{}", name),
@@ -79,6 +136,29 @@ impl Expr {
             Expr::AddrOf(e) => {
                 write!(fmt, "&(")?;
                 e.as_ref().fmt(fmt)?;
+                write!(fmt, ")")
+            }
+            Expr::FieldAccess { var, field } => {
+                var.as_ref().fmt(fmt)?;
+                if var.is_ptr() {
+                    write!(fmt, "->{}", field)
+                } else {
+                    write!(fmt, ".{}", field)
+                }
+            }
+            Expr::MethodCall { var, method, args } => {
+                var.as_ref().fmt(fmt)?;
+                if var.is_ptr() {
+                    write!(fmt, "->{}(", method)?;
+                } else {
+                    write!(fmt, ".{}(", method)?;
+                }
+                for (i, v) in args.iter().enumerate() {
+                    if i != 0 {
+                        write!(fmt, ", ")?;
+                    }
+                    v.fmt(fmt)?;
+                }
                 write!(fmt, ")")
             }
             Expr::BinOp { lhs, rhs, op } => {
